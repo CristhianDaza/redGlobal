8ee11c2f40a27e29b0d90fa5d501badb03185c9b
Cristhian Daza <cristian.d.b@hotmail.com>
2025-03-03 20:56:13 -0500

    chore(*): init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Red Global</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import RedGlobal from './App.vue'
+
+createApp(RedGlobal).mount('#redGlobal')
