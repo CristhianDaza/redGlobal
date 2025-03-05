b4b52ed2b4c15ec35ad8d803d2f8039b31a3ed57
Cristhian Daza <cristian.d.b@hotmail.com>
2025-03-05 16:25:31 -0500

    feat(firebase): add Firebase configuration and initialize services

---
+++ firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+}
+
+const app = initializeApp(firebaseConfig)
+
+const analytics = getAnalytics(app)
+const db = getFirestore(app)
+const storage = getStorage(app, import.meta.env.VITE_BACKET_URL)
+
+export {
+  analytics,
+  db,
+  storage,
+}
