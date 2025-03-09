2188e1132f0b735d9f611ffd54342b294c4c16b4
Cristhian Daza <cristian.d.b@hotmail.com>
2025-03-08 20:56:53 -0500

    feat(apiPromos): rename stock fetching functions for clarity

--- src/api/apiPromos.js
+++ src/api/apiPromos.js
@@ -10,7 +10,7 @@ const userData = {
  * @description This POST request is used to obtain the Stock list of all active products. It is displayed by Products, Stock and Warehouse.
  * @returns {Promise<unknown>}
  */
-export const getAllStock = async () => {
+export const getStockPromos = async () => {
   try {
     return await apiService.post(apiConfigPromos, 'all-stocks', userData);
   } catch (error) {
@@ -24,7 +24,7 @@ export const getAllStock = async () => {
  * @param {string} sku - Product SKU
  * @returns {Promise<unknown>}
  */
-export const getProductStock = async (sku) => {
+export const getStockByIdPromos = async (sku) => {
   try {
     return await apiService.post(apiConfigPromos, 'all-stocks', { ...userData, sku });
   } catch (error) {
@@ -37,7 +37,7 @@ export const getProductStock = async (sku) => {
  * @description The POST request is built to obtain the list of all products with their technical data sheet. This information is found at the level of the parent product and only the variants are found at the level of the child product.
  * @returns {Promise<*|undefined>}
  */
-export const getAllProducts = async () => {
+export const getAllProductsPromos = async () => {
   try {
     return await apiService.post(apiConfigPromos, 'all-products', userData);
   } catch (error) {
