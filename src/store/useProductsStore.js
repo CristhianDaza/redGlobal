0920e02bc45e0d850c40a5c355aedd24f3504a59
Cristhian Daza <cristian.d.b@hotmail.com>
2025-03-12 19:54:50 -0500

    feat(useProductsStore): enhance product management with Firebase integration and batch processing

--- src/store/useProductsStore.js
+++ src/store/useProductsStore.js
@@ -1,4 +1,8 @@
 import { defineStore } from 'pinia'
+import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
+import { db } from '../../firebase.js'
+import { combineProducts } from '../utils'
+
 import {
   // Promos API
   getProductsPromos,
@@ -24,9 +28,30 @@ export const useProductsStore = defineStore('products', {
     isLoadingApiMarpico: false,
     isLoadingApiStockSur: false,
     isLoadingApiCataProm: false,
+    isLoadingSaveProducts: false,
+    lastUpdateProducts: null,
   }),
   actions: {
-    async getAllProducts() {
+    async getAllProducts(isAdminUser = false) {
+      try {
+        if (isAdminUser) {
+          await this._callServices()
+        } else {
+          await this._callProductsDb()
+        }
+      } catch (error) {
+        console.error('Error in getAllProducts:', error)
+      }
+    },
+    
+    async _callServices() {
+      const isMustBeUpdated = await this._isMustBeUpdated()
+      
+      if (isMustBeUpdated) {
+        await this._callProductsDb()
+        return
+      }
+
       this.isLoadingApiPromos = isLoadingProductsPromosComposable
       this.isLoadingApiMarpico = isLoadingProductsMarpicoComposable
       this.isLoadingApiStockSur = isLoadingProductsStockSurComposable
@@ -49,7 +74,79 @@ export const useProductsStore = defineStore('products', {
         ...productsStockSur,
         ...productsCataProm
       ]
-      this.products = allProducts.sort((a, b) => a.name.localeCompare(b.name))
-    }
+      const products = allProducts.sort((a, b) => a.name.localeCompare(b.name))
+      
+      this.isLoadingSaveProducts = false
+      await this._saveProductsInDb(products)
+    },
+    
+    async _saveProductsInDb(allProducts) {
+      this.isLoadingSaveProducts = true
+      await this._deleteProductsInDb()
+      const batchSizes = 500
+      
+      for (let i = 0; i < allProducts.length; i += batchSizes) {
+        const batch = allProducts.slice(i, i + batchSizes).map(product => {
+          return JSON.parse(JSON.stringify(product, (key, value) =>
+            value === undefined ? null : value
+          ))
+        })
+        await addDoc(collection(db, 'productsRedGlobal'), { products: batch })
+      }
+      await this._updateDateInDb()
+      await this._callProductsDb()
+    },
+    
+    async _callProductsDb() {
+      const docRef = await getDocs(collection(db, 'productsRedGlobal'))
+      
+      const docRefLastUpdated = await getDocs(collection(db, 'lastedUpdatedProducts'))
+      const { lastUpdate } = docRefLastUpdated.docs[0].data()
+      this.lastUpdateProducts = lastUpdate
+      const allNormalizedProducts = combineProducts(docRef.docs)
+      console.log('allNormalizedProducts:', allNormalizedProducts)
+      this.products = allNormalizedProducts.sort((a, b) => a.name.localeCompare(b.name))
+      this.isLoadingSaveProducts = false
+    },
+    
+    async _updateDateInDb() {
+      try {
+        await addDoc(collection(db, 'lastedUpdatedProducts'), { lastUpdate: new Date().toISOString() })
+      } catch (error) {
+        console.log('Error in _updateDateInDb:', error)
+      }
+    },
+    
+    async _deleteProductsInDb() {
+      try {
+        const productsRef = await getDocs(collection(db, 'productsRedGlobal'))
+        const lastUpdateRef = await getDocs(collection(db, 'lastedUpdatedProducts'))
+        
+        const deleteLastUpdate = lastUpdateRef.docs.map(document => deleteDoc(doc(db, 'lastedUpdatedProducts', document.id)))
+        
+        const deletePromises = productsRef.docs.map(document => deleteDoc(doc(db, 'productsRedGlobal', document.id)))
+        
+        if (deletePromises.length === 0 && deleteLastUpdate.length === 0) {
+          return
+        }
+        await Promise.all([...deletePromises, ...deleteLastUpdate])
+      } catch (error) {
+        console.log('Error in _deleteProductsInDb:', error)
+      }
+    },
+    
+    async _isMustBeUpdated() {
+      const docRef = await getDocs(collection(db, 'lastedUpdatedProducts'))
+      if (!docRef.docs || docRef.docs.length === 0) {
+        return false
+      }
+      const { lastUpdate } = docRef.docs[0].data()
+      const lastUpdateDate = new Date(lastUpdate)
+      const now = new Date()
+      const lastUpdateDay = lastUpdateDate.getDate()
+      const nowDay = now.getDate()
+      this.lastUpdateProducts = lastUpdateDate
+      return lastUpdateDay === nowDay
+    },
   },
 })
