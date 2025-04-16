f6f18419e9e5014becfaae48a6d32d7486dd0a25
Cristhian Daza <cristian.d.b@hotmail.com>
2025-04-16 13:06:42 -0500

    feat(catalog): update CatalogsSection to improve route display and enhance image styling

--- src/components/Admin/sections/CatalogsSection.vue
+++ src/components/Admin/sections/CatalogsSection.vue
@@ -30,7 +30,7 @@ defineEmits<{
         <thead>
         <tr>
           <th>Nombre</th>
-          <th>Ruta</th>
+          <th>Ver Ruta</th>
           <th>Imagen</th>
           <th>Acciones</th>
         </tr>
@@ -39,10 +39,20 @@ defineEmits<{
         <tr v-for="item in items" :key="item.id">
           <td>{{ item.title }}</td>
           <td>
-            <a :href="item.toRoute" target="_blank">{{ item.toRoute }}</a>
+            <a :href="item.toRoute" target="_blank">
+              <RgButton
+                icon="share"
+                type="icon"
+                outlined
+                :customStyle="{
+                  backgroundColor: '#335d80',
+                  color: '#ebf8ff',
+                }"
+              />
+            </a>
           </td>
           <td>
-            <img :src="item.imageUrl" alt="Vista previa" class="image-preview">
+            <img :src="item.imageUrl" alt="Vista previa" class="card-thumbnail">
           </td>
           <td class="actions">
             <RgButton
@@ -74,5 +84,10 @@ defineEmits<{
 </template>
 
 <style scoped>
-
+.card-thumbnail {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 0.375rem;
+}
 </style>
