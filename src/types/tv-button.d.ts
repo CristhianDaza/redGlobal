534825110c080bb38c943c182631d61d46185c8a
Cristhian Daza <cristian.d.b@hotmail.com>
2025-05-12 20:30:21 -0500

    chore: add type declarations for TodoVue components and configure typeRoots

---
+++ src/types/tv-button.d.ts
@@ -0,0 +1,7 @@
+// src/types/tv-button.d.ts
+
+declare module '@todovue/tv-button' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+} 
---
+++ src/types/tv-relative-time.d.ts
@@ -0,0 +1,7 @@
+// src/types/tv-relative-time.d.ts
+
+declare module '@todovue/tv-relative-time' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+} 
