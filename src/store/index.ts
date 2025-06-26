761e3e5ec9e7893792cdfc0b3e7eeafddcaa9745
Cristhian Daza <cristian.d.b@hotmail.com>
2025-06-25 21:41:07 -0500

    feat(loader): add global loader component and integrate loading state management

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,3 +5,4 @@ export { useCategoryStore } from '@/store/useCategoryStore';
 export { useUserStore } from '@/store/useUserStore';
 export { useQuoteStore } from '@/store/useQuoteStore';
 export { useCatalogStore } from '@/store/useCatalogStore.ts';
+export { useLoaderStore } from '@/store/useLoaderStore.ts';
